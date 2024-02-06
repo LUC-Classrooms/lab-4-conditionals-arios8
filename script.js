9f43d2c60126c9b52e9b7273b506b0e252b814e8
arios8 <154016447+arios8@users.noreply.github.com>
2024-02-06 15:10:29 +0000

    Lab 4 submitted

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
+    fill(51, 84, 60);
+    ellipse(width/2, height/2, 200, 230);
   }
   else { 
     // otherwise do this:
-    
+    fill(144, 163, 144);
   }
   ellipse(width/2, height/2, 100, 100);
+  stroke(53, 66, 57);
 }
